3880816afdd611e8a82b56bd3973378618529d35
yo9299 <yo9299@gmail.com>
2023-03-07 18:20:11 +0100

    Fermat test and deterministic one to see if a number is prime

---
+++ PrimeChecker.hs
@@ -0,0 +1,25 @@
+module PrimeChecker where 
+import Data.Bits
+
+--calcule b^e (mod m) From trevordixon/modExp.hs
+modExp :: Integer -> Integer -> Integer -> Integer 
+modExp b 0 m = 1
+modExp b e m = t * modExp ((b*b) `mod` m) (shiftR e 1) m `mod` m 
+    where t = if testBit e 0 then b `mod` m else 1
+
+testFermat:: Integer -> Bool
+testFermat a 
+    | results == [1,1,1,1] = True
+    | otherwise = False
+    where results = [modExp j (a-1) a | j <- [2,3,5,7]]
+
+isDivisible :: Integer -> Integer -> Bool
+isDivisible n a 
+    | n `mod` a == 0 = True 
+    | otherwise = False
+
+deterministicTest :: Integer -> Bool
+deterministicTest a = foldr1 (||) (map f [2..root] )
+    where root =  a-1
+          --h div1 div2 = (isDivisible a div1 ) || (isDivisible a div2 )
+          f div1 = isDivisible a div1
