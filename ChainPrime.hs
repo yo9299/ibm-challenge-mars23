962391020ea2c3038c64d4bcd25de1d038230ea6
yo9299 <yo9299@gmail.com>
2023-03-08 11:04:02 +0100

    changed stopping condition, still not working

--- ChainPrime.hs
+++ ChainPrime.hs
@@ -7,13 +7,22 @@ newDigit :: Integer -> Integer -> Integer
 newDigit a b = a*10 + b
 
 --this function returns the list of all the chained primes with five exceptions
-createChain :: (Integer, Integer)->  [(Integer, Integer)] 
-createChain (a ,5) = zip (stopCondition a) (repeat 5)
+{-createChain :: (Integer, Integer)->  [(Integer, Integer)] 
+createChain (a ,5) | stopCondition a == [] = (a,5)
+                   | otherwise zip (stopCondition a) (repeat 5)
 --createChain a b = Just concat [createChain ((newDigit a j), (f (newDigit a j) b)) | j <- [0..9]]
 createChain (a, b) = concat $ fmap (createChain) [(newDigit a j ,f (newDigit a j) b) | j <- [0..9]]
     where  f dig ac 
                    | not $ testFermat dig = ac +1
-                   | otherwise = ac
+                   | otherwise = ac-}
+
+createChain :: (Integer, Integer)->  [(Integer, Integer)] 
+createChain (a ,b) | (b ==5) && (stopCondition a == []) = [(a,5)]
+                   | (b ==5) && (stopCondition a /= []) = zip (stopCondition a) (repeat 5)
+                   | otherwise = concat $ fmap (createChain) [(newDigit a j ,f (newDigit a j) b) | j <- [0..9]]
+                        where  f dig ac 
+                                        | not $ testFermat dig = ac +1
+                                        | otherwise = ac
 
 --bigChain :: [(Integer, Integer)]  -> [(Integer, Integer)]
 --bigChain [(a, b)] = concat $ fmap (createChain) [(a, b)]
@@ -23,7 +32,7 @@ addPrimes a = filter (testFermat) [ newDigit a b | b <- [1,3,7,9]]
 
 stopCondition :: Integer -> [Integer]
 stopCondition a
-                | addPrimes a == [] = [a]
+                | addPrimes a == [] = []
                 | otherwise = concat $ fmap (stopCondition) (addPrimes a)
 
 --start creating a chain: if 
@@ -34,4 +43,4 @@ largestPrime [(a, b)] = foldr1 f (map (fst) (concat $ fmap  (createChain) [(a,b)
                             --L.maximumBy h [num1, num2] h num1 num2 = num1 
                      
 initialList :: [(Integer, Integer)] 
-initialList = [(i, 0) | i <-[0..9]]
+initialList = [(i, 0) | i <-[2,3,5,7]] ++ [(i, 1) | i <-[0,1,4,5,6,8,9]]
