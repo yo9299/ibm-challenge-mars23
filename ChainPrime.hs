f274899fde0e669feb9bb1e875e9234e067d6844
yo9299 <yo9299@gmail.com>
2023-03-26 15:01:10 +0200

    add new file LArgestChain

--- ChainPrime.hs
+++ ChainPrime.hs
@@ -1,10 +1,14 @@
 import PrimeChecker
 import Data.Maybe
 import qualified Data.List as L
+import Debug.Trace
 
 
 newDigit :: Integer -> Integer -> Integer 
 newDigit a b = a*10 + b
+
+newDigitLeft :: Integer -> Integer -> Integer
+newDigitLeft a b = a + b* 10
 
 --this function returns the list of all the chained primes with five exceptions
 {-createChain :: (Integer, Integer)->  [(Integer, Integer)] 
@@ -16,16 +20,23 @@ createChain (a, b) = concat $ fmap (createChain) [(newDigit a j ,f (newDigit a j
                    | not $ testFermat dig = ac +1
                    | otherwise = ac-}
 
-createChain :: (Integer, Integer)->  [(Integer, Integer)] 
-createChain (a ,b) | (b ==5) && (stopCondition a == []) = [(a,5)]
-                   | (b ==5) && (stopCondition a /= []) = zip (stopCondition a) (repeat 5)
-                   | otherwise = concat $ fmap (createChain) [(newDigit a j ,f (newDigit a j) b) | j <- [0..9]]
-                        where  f dig ac 
-                                        | not $ testFermat dig = ac +1
+createChain :: (Integer, Integer) -> Integer ->  ([(Integer, Integer)], Integer)
+createChain (a ,ex) best 
+                        | (ex ==5) && (stopCondition a == []) = trace(show max1) ([(a,5)], max1)
+                        | (ex ==5) && (stopCondition a /= []) = trace(show max2) (lastPrime, max2)
+                        | otherwise =  trace(show max3) (lastEx, max3)
+                            where  f dig ac 
+                                        | not $ testFermat dig = ac + 1
                                         | otherwise = ac
+                                   lastPrime = zip (stopCondition a) (repeat 5)
+                                   max1 = max  (maximum $ fmap fst [(a,5)]) best
+                                   max2 = max  (maximum $ fmap fst lastPrime) best
+                                   max3 = max (maximum $ bests) best
+                                   (lastEx', bests) = unzip $ (zipWith (createChain) [(newDigit a j ,f (newDigit a j) ex) | j <- [0..9]] (repeat best) )
+                                   lastEx = concat lastEx'
+                                  
+                                   
 
---bigChain :: [(Integer, Integer)]  -> [(Integer, Integer)]
---bigChain [(a, b)] = concat $ fmap (createChain) [(a, b)]
 
 addPrimes :: Integer -> [Integer]
 addPrimes a = filter (testFermat) [ newDigit a b | b <- [1,3,7,9]]
@@ -38,9 +49,9 @@ stopCondition a
 --start creating a chain: if 
 
 largestPrime :: [(Integer, Integer)] -> Integer
-largestPrime l= maximum $ (map (fst) (concat $ fmap  (createChain) l ))
-                        --where f num1 num2 = max num1 num2
-                            --L.maximumBy h [num1, num2] h num1 num2 = num1 
+largestPrime l= maximum bests 
+                    where (result, bests )=  unzip $  fmap  (flip createChain 0) l 
+
                      
 initialList :: [(Integer, Integer)] 
 initialList = [(i, 0) | i <-[2,3,5,7]] ++ [(i, 1) | i <-[0,1,4,5,6,8,9]]
--- LargestChain.hs
+++ LargestChain.hs
@@ -15,9 +15,10 @@ newDigitLeft a b = a + b* 10
 createChain :: (Integer, Integer) -> (Integer, Integer)
 --given an int, I will call recursively newdigit that counts the exceptions until i can't add no more. 
 -- then, I will store the result and i will keep only the best
-createChain (p, 6) =  (stopCondition p, 6)
-createChain (p, counter) = trace(show maxsofar) maxsofar
+createChain (p, 5) =  (stopCondition p, 5)
+createChain (p, counter) = trace(show track) maxsofar
     where newinput = [(newDigit p j ,f (newDigit p j) counter) | j <- [0..9]]
+          track = max p (fst maxsofar)
           maxsofar = maximumBy (comparing fst) (fmap (createChain) newinput)
           --max1 = stopCondition p
           f dig ac 
@@ -37,4 +38,13 @@ stopCondition a
 
 
 initialList :: [(Integer, Integer)] 
-initialList = [(i, 0) | i <-[2,3,5,7]] ++ [(i, 1) | i <-[0,1,4,6,8,9]]
+initialList = [(i, 0) | i <-[2,3,5,7]] 
+
+initialList2 :: [(Integer, Integer)]
+initialList2 = [(i, 1) | i <-[1,4,6,8,9]]
+
+--78797339359331171479990333 26
+--238339393727993993999713991 27
+-- 3733799911799539139382193991 28
+-- 5939337193981799393983973 25
+-- 972137331717733217999
